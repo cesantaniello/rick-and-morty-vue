c274480f1bbb6e499fa82bba0a8978c36230aa33
Administrator <admin@example.com>
2022-03-17 14:11:26 +0100

    2. Obten personajes

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,29 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    characters: [],
+    charactersFilter: []
   },
   getters: {
   },
   mutations: {
+    setCharacters (state, payload) {
+      state.characters = payload
+    },
+    setCharactersFilter (state, payload) {
+      state.charactersFilter = payload
+    }
   },
   actions: {
+    async getCharacters ({ commit }) {
+      try {
+        const response = await fetch('https://rickandmortyapi.com/api/character/')
+        const data = await response.json()
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
   },
   modules: {
   }
