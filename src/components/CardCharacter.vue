8a8f8b2e2e21fe0b18710cf2a1a2da516bb03448
Administrator <admin@example.com>
2022-03-17 16:59:59 +0100

    4. Crea componente de Card

---
+++ src/components/CardCharacter.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="character">
+    <img :src="character.image" :alt="character.name">
+    <div class="character__info">
+      <h3>{{ character.name }}</h3>
+      <div class="status">
+        <span
+          :class="
+            character.status === 'Alive' ? 'alive' :
+            character.status === 'Dead' ? 'dead' :
+            'default'
+          "
+        ></span>
+        <span>{{ character.status }} - {{ character.species }}</span>
+      </div>
+      <div class="origin">
+        <span>Origin: {{ character.origin.name }}</span>
+      </div>
+      <div class="location">
+        <span>Location: {{ character.location.name }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['character'],
+}
+</script>
+
+<style>
+
+</style>
--- src/components/ListCharacters.vue
+++ src/components/ListCharacters.vue
@@ -2,7 +2,7 @@
   <section>
     <div class="characters">
       <div class="characters__item" v-for="character in characters" :key="character.id">
-        {{ character.name }}
+        <CardCharacter :character="character" />
       </div>
     </div>
   </section>
@@ -11,8 +11,12 @@
 <script>
 import { onMounted, computed } from 'vue';
 import { useStore } from 'vuex';
+import CardCharacter from '@/components/CardCharacter.vue';
 
 export default {
+  components: {
+    CardCharacter,
+  },
   setup(){
     const store = useStore();
     const characters = computed(() => {
